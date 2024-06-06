6b2dc1e853d79f368f4033a4a1d72e7823b44074
Alice Perim <109715359+aliceperim@users.noreply.github.com>
2024-06-06 10:05:32 -0300

    Update sistemaBancario.py

--- sistemaBancario.py
+++ sistemaBancario.py
@@ -1,31 +1,31 @@
 menu = """
 
-[d] Depositar
-[s] Sacar
+[d] Depósito
+[s] Saque
 [e] Extrato
 [q] Sair
 
 => """
 
 saldo = 0
-limite = 500
-extrato = ""
+limite = 1000
+ext = ""
 numero_saques = 0
-LIMITE_SAQUES = 3
+LIMITE_SAQUES = 5
 
 while True:
 
     opcao = input(menu)
 
     if opcao == "d":
-        valor = float(input("Informe o valor do depósito: "))
+        valor = float(input("Informe o valor a ser depositado: "))
 
         if valor > 0:
             saldo += valor
-            extrato += f"Depósito: R$ {valor:.2f}\n"
+            ext += f"Depósito: R$ {valor:.2f}\n"
 
         else:
-            print("Operação falhou! O valor informado é inválido.")
+            print("O valor informado é inválido.")
 
     elif opcao == "s":
         valor = float(input("Informe o valor do saque: "))
@@ -37,25 +37,25 @@ while True:
         excedeu_saques = numero_saques >= LIMITE_SAQUES
 
         if excedeu_saldo:
-            print("Operação falhou! Você não tem saldo suficiente.")
+            print("Você não tem saldo suficiente.")
 
         elif excedeu_limite:
-            print("Operação falhou! O valor do saque excede o limite.")
+            print("O valor do saque excede o limite.")
 
         elif excedeu_saques:
-            print("Operação falhou! Número máximo de saques excedido.")
+            print("Número máximo de saques excedido.")
 
         elif valor > 0:
             saldo -= valor
-            extrato += f"Saque: R$ {valor:.2f}\n"
+            ext += f"Saque: R$ {valor:.2f}\n"
             numero_saques += 1
 
         else:
-            print("Operação falhou! O valor informado é inválido.")
+            print("O valor informado é inválido.")
 
     elif opcao == "e":
         print("\n================ EXTRATO ================")
-        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print("Sem movimentações." if not extrato else extrato)
         print(f"\nSaldo: R$ {saldo:.2f}")
         print("==========================================")
 
